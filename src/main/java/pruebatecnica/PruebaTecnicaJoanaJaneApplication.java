cb487652eeaaef0ef275f881013a1c09117e93af
Joana <joanajanefdez@gmail.com>
2022-08-23 09:44:09 +0200

    config inicial + security

---
+++ src/main/java/pruebatecnica/PruebaTecnicaJoanaJaneApplication.java
@@ -0,0 +1,13 @@
+package pruebatecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaJoanaJaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaJoanaJaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pruebatecnica/PruebaTecnicaJoanaJaneApplicationTests.java
@@ -0,0 +1,13 @@
+package pruebatecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaJoanaJaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
